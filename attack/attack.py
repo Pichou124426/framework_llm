7099b9999cf79551990620b367a4773b5a92ae5f
Noah <noah.sillaire@cgi.com>
2026-07-21 16:35:13 +0200

    Ajout de property afin d'avoir acces a l'autoimplementation lors de l'utiilsation de self.rag

--- attack/attack.py
+++ attack/attack.py
@@ -5,6 +5,7 @@ from common.indexation import Indexeur
 from common.initialisation import Initialisation
 from common.rag_config import RagConfig
 from dataclasses import asdict
+from typing import cast
 
 class Attack:
     def __init__(self, name, description, target: RagConfig):
@@ -20,3 +21,17 @@ class Attack:
             self.rag["indexeur"] = Indexeur(**asdict(target.indexeur_config))
         if target.initialisation_config:
             self.rag["initialisation"] = Initialisation(**asdict(target.initialisation_config))
+    @property
+    def retriever(self) -> Retriever:
+        return cast(Retriever, self.rag["retriever"])
+
+    @property
+    def generation(self) -> Generation:
+        return cast(Generation, self.rag["generation"])
+
+    @property
+    def indexeur(self) -> Indexeur:
+        return cast(Indexeur, self.rag["indexeur"])
+    @property
+    def initialisation(self) -> Initialisation:
+        return cast(Initialisation, self.rag["initialisation"])
